b72407892bb415f654f175645bb2b9658d9425f8
banghehe <phamduyentba@gmail.com>
2022-11-12 19:36:18 +0700

    Update code

--- app/src/main/java/com/quanglong/recipeapp/fragments/HomeFragment.java
+++ app/src/main/java/com/quanglong/recipeapp/fragments/HomeFragment.java
@@ -1,20 +1,26 @@
 package com.quanglong.recipeapp.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.quanglong.recipeapp.R;
+import com.quanglong.recipeapp.activities.CatagoryActivity;
 
 /**
  * A simple {@link Fragment} subclass.
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
+    ImageView imageView;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -23,7 +29,27 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        imageView = view.findViewById(R.id.ic_arrow_right);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),CatagoryActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void doInitialization() {
+//        this.imageView = imageView.findViewById(R.id.ic_arrow_right);
+
     }
 }

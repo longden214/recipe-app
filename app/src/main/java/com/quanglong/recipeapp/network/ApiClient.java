e3d29adf64b82175cda613dca3e52cce9d979e39
longden <nhokimlien1411@gmail.com>
2022-11-29 08:43:04 +0700

    update code

--- app/src/main/java/com/quanglong/recipeapp/network/ApiClient.java
+++ app/src/main/java/com/quanglong/recipeapp/network/ApiClient.java
@@ -17,7 +17,7 @@ public class ApiClient {
                     .create();
 
             retrofit = new Retrofit.Builder()
-                    .baseUrl("http://172.22.32.1:8080/APIRecipes/recipesApi/")
+                    .baseUrl("http://172.19.128.1:8080/APIRecipes/recipesApi/")
                     .addConverterFactory(ScalarsConverterFactory.create())
                     .addConverterFactory(GsonConverterFactory.create(gson))
                     .build();
